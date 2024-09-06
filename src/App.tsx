d35f7c1321490689e8900317c121ae7638ca3778
Cornel <ifeanyichukwu.c.ikechukwu@gmail.com>
2024-09-06 18:41:53 +0100

    CatBox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import CatBox from "./components/CatBox";
-import CatController from "./components/catController";
+import BearBox from "./components/BearBox";
+// import CatBox from "./components/CatBox";
+// import CatController from "./components/catController";
 
 function App() {
   return (
     <div className="container">
       <h1>Zustand Tutorial</h1>
       <div>
-        <CatBox />
-        <CatController />
+        {/* <CatBox />
+        <CatController /> */}
+        <BearBox />
       </div>
     </div>
   );
