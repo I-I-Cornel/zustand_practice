f01637ae074229f0603e30b52e83bf30c3240302
Cornel <ifeanyichukwu.c.ikechukwu@gmail.com>
2024-09-09 15:19:38 +0100

    CatBox component

--- src/stores/catStore.ts
+++ src/stores/catStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand"
 import { immer } from "zustand/middleware/immer"
 import { createSelectors } from "../utils/createSelectors"
-import { devtools } from "zustand/middleware"
+import { devtools, persist } from "zustand/middleware"
 
 type TCatStoreState = {
     cats: {
@@ -15,27 +15,34 @@ type TCatStoreState = {
 }
 
 export const useCatStore = createSelectors(create<TCatStoreState>()
-(immer(devtools((set, get) => ({
-    cats: {
-        bigCats: 0,
-        smallCats: 0,
-    },
-    increaseBigCats: () =>{set((state) => {
-        state.cats.bigCats ++;
-    })},
-    increaseSmallCats: () =>{set((state) => {
-        state.cats.smallCats ++;
-    })},
-    totalCats: () => {
-        const totalCats = get().cats.bigCats + get().cats.smallCats;
-        return `There are ${totalCats} cats in total`
-    },
-    removeAllCats: () => {set((state) => {
-        state.cats.smallCats = 0;
-        state.cats.bigCats = 0;
-    })}
-}),
-{
-    enabled: true,
-    name: "cat store"
-}))));
+(immer
+    (devtools(
+        persist((set, get) => ({
+            cats: {
+                bigCats: 0,
+                smallCats: 0,
+            },
+            increaseBigCats: () =>{set((state) => {
+                state.cats.bigCats ++;
+            })},
+            increaseSmallCats: () =>{set((state) => {
+                state.cats.smallCats ++;
+            })},
+            totalCats: () => {
+                const totalCats = get().cats.bigCats + get().cats.smallCats;
+                return `There are ${totalCats} cats in total`
+            },
+            removeAllCats: () => {set((state) => {
+                state.cats.smallCats = 0;
+                state.cats.bigCats = 0;
+            })}
+            }), {
+                name: "cat store",
+            }
+        ),
+    {
+        enabled: true,
+        name: "cat store"
+    }
+    ))
+));
