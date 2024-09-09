97d1a5acfe648d020b13977f1a19e4e49b1c9fed
Cornel <ifeanyichukwu.c.ikechukwu@gmail.com>
2024-09-09 15:22:58 +0100

    CatBox component

--- src/stores/bearStore.ts
+++ src/stores/bearStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { devtools } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 
 type TBearStoreState = {
   bears: number,
@@ -7,11 +7,15 @@ type TBearStoreState = {
   removeAllBears: () => void,
 }
 
-export const useBearStore = create<TBearStoreState>()(devtools((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-}), {
-  enabled: true,
-  name: "bear store",
-}))
+export const useBearStore = create<TBearStoreState>()(devtools(
+  persist(
+    (set) => ({
+      bears: 0,
+      increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+      removeAllBears: () => set({ bears: 0 }),
+    }), {
+      name: 'bear store',
+    }), {
+      enabled: true,
+      name: "bear store",
+  }))
